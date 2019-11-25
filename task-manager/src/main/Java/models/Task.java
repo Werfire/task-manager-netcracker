f01684c02ba97468a5d7cfcfad5e6c6ea209dc5b
SwarrenB <sovasyava14@gmail.com>
2019-11-26 01:34:20 +0400

    Equals & Hash & ToString

--- task-manager/src/main/Java/models/Task.java
+++ task-manager/src/main/Java/models/Task.java
@@ -1,8 +1,11 @@
 package models;
 
+import com.google.common.base.MoreObjects;
+
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
+import static com.google.common.base.Objects.*;
 public class Task implements Serializable {
     private int id;
     private String name;
@@ -51,27 +54,55 @@ public class Task implements Serializable {
     }
 
     @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        Task task = (Task) object;
-        return id == task.id &&
-                authorId == task.authorId &&
-                statusId == task.statusId &&
-                Objects.equals(name, task.name) &&
-                Objects.equals(description, task.description) &&
-                Objects.equals(creationDate, task.creationDate) &&
-                Objects.equals(dueDate, task.dueDate);
+    public int hashCode() {
+        return Objects.hash(id,name,description,creationDate,dueDate,authorId,statusId);
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(id, name, description, creationDate, dueDate, authorId, statusId);
+    public boolean equals(Object obj) {
+       if(obj == null || getClass() != obj.getClass())
+           return false;
+       Task other = (Task) obj;
+       return Objects.equals(id,other.id) && Objects.equals(name,other.name) && Objects.equals(description,other.description)
+               && Objects.equals(creationDate,other.creationDate) && Objects.equals(dueDate,other.dueDate) &&
+               Objects.equals(authorId,other.authorId) && Objects.equals(statusId,other.statusId);
     }
 
     @Override
     public String toString() {
-        return (id + "\t" + name + "\t" + description + "\t" + creationDate.toString() + "\t" +
-                dueDate.toString() + "\t" + authorId + "\t" + statusId + "\n");
+        return MoreObjects.toStringHelper(this)
+                .omitNullValues()
+                .add("id", id)
+                .add("name", name)
+                .add("description", description)
+                .add("creationDate", creationDate)
+                .add("dueDate", dueDate)
+                .add("authorId", authorId)
+                .add("statusId", statusId)
+                .toString();
     }
+//    @Override
+//    public boolean equals(Object object) {
+//        if (this == object) return true;
+//        if (object == null || getClass() != object.getClass()) return false;
+//        Task task = (Task) object;
+//        return id == task.id &&
+//                authorId == task.authorId &&
+//                statusId == task.statusId &&
+//                Objects.equals(name, task.name) &&
+//                Objects.equals(description, task.description) &&
+//                Objects.equals(creationDate, task.creationDate) &&
+//                Objects.equals(dueDate, task.dueDate);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id, name, description, creationDate, dueDate, authorId, statusId);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return (id + "\t" + name + "\t" + description + "\t" + creationDate.toString() + "\t" +
+//                dueDate.toString() + "\t" + authorId + "\t" + statusId + "\n");
+//    }
 }
