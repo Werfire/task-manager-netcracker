bb0f2e7c282b5cf8d47ce9c1ac907d7f4248b1d9
Maksimonchik <maksimonchik@gmail.com>
2019-11-26 01:31:32 +0400

    Date changed to LocalDateTime and added Serialization Test

--- task-manager/src/main/Java/Main.java
+++ task-manager/src/main/Java/Main.java
@@ -1,14 +1,7 @@
-import controllers.TaskController;
-import models.Task;
-
 import java.io.IOException;
-import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        TaskController controller = new TaskController();
-        controller.add(new Task(1, "Cleaning", "Clean living room", new Date(), new Date(),
-                5, 3));
-        controller.write();
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
     }
 }
