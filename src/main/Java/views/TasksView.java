943a2b6ece5547880e7d97a3c298523227bcdcae
SwarrenB <sovasyava14@gmail.com>
2019-12-04 23:58:39 +0400

    GR

--- src/main/Java/views/TasksView.java
+++ src/main/Java/views/TasksView.java
@@ -1,38 +1,34 @@
 package views;
 import javax.swing.*;
-import javax.swing.plaf.metal.MetalBorders;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TasksView extends JFrame {
 
     private JPanel mainPanel;
-    private JTextField taskNameField;
-    private JTextField dueDateField;
-    private JTable tasksTable;
-    private JButton addTaskButton;
+    private JTable table1;
+    private JButton DELETEButton;
+    private JButton ADDButton;
+    private JButton EDITButton;
+    private JButton COMPLETEButton;
+
 
     public TasksView() {
         add(mainPanel);
         setTitle("Task Manager");
-        setSize( 550, 320);
+        setSize( 800, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
+        setResizable(false); // нельзя менять размеры окна
 
-        //TableModel tableModel =
-        //        new DefaultTableModel(new Object[][] {{"Задача", "Сделать задачу.", "2019 12.12 12:00"}},
-        //                new String[] {"Name", "Description", "Due date"});
-        //tasksTable.setModel(tableModel);
-
-        addTaskButton.addActionListener(new ActionListener() {
+        ADDButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
 
             }
         });
+    }
 
-        setLocationRelativeTo(null);
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
