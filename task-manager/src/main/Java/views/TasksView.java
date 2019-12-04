f0f314e0186ce3e37b69fa32a13afe7f37b317ea
Werfire <maksimonchik@gmail.com>
2019-12-04 23:56:45 +0400

    Added input errors

--- task-manager/src/main/Java/views/TasksView.java
+++ task-manager/src/main/Java/views/TasksView.java
@@ -1,22 +1,30 @@
 package views;
 import javax.swing.*;
+import javax.swing.plaf.metal.MetalBorders;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TasksView extends JFrame {
 
     private JPanel mainPanel;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTable table1;
+    private JTextField taskNameField;
+    private JTextField dueDateField;
+    private JTable tasksTable;
     private JButton addTaskButton;
 
     public TasksView() {
         add(mainPanel);
         setTitle("Task Manager");
-        setSize( 400, 200);
+        setSize( 550, 320);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false); // нельзя менять размеры окна
+        setResizable(false);
+
+        //TableModel tableModel =
+        //        new DefaultTableModel(new Object[][] {{"Задача", "Сделать задачу.", "2019 12.12 12:00"}},
+        //                new String[] {"Name", "Description", "Due date"});
+        //tasksTable.setModel(tableModel);
 
         addTaskButton.addActionListener(new ActionListener() {
             @Override
@@ -24,5 +32,7 @@ public class TasksView extends JFrame {
 
             }
         });
+
+        setLocationRelativeTo(null);
     }
 }
