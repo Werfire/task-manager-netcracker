2ce1331217198bcca3c54e82e48b43d7059816e1
Werfire <maksimonchik@gmail.com>
2019-12-20 02:03:14 +0400

    Main cleanup

--- src/main/Java/Main.java
+++ src/main/Java/Main.java
@@ -10,13 +10,9 @@ import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-//        JFrame.setDefaultLookAndFeelDecorated(true);
-//        JDialog.setDefaultLookAndFeelDecorated(true);
-
         UsersController usersController = new UsersController();
         TasksController tasksController = new TasksController();
-//        TasksView tasksView = new TasksView(usersController, tasksController, new User(UUID.randomUUID(), "User", "qwerty"));
-        LoginView log = new LoginView(usersController, tasksController);
+        LoginView loginView = new LoginView(usersController, tasksController);
 
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             public void run() {
