13c9659b75ad688aac631c5338d4ce7b99d6bcc0
Werfire <maksimonchik@gmail.com>
2020-01-23 14:36:49 +0400

    Started REST

--- src/main/Java/net/REST.java
+++ src/main/Java/net/REST.java
@@ -1,4 +1,20 @@
 package net;
 
+import models.MutableTask;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.UUID;
+
 public class REST {
+
+//    @Path("tasks")
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public HashMap<UUID, MutableTask> getTasksJournal() {
+//
+//    }
 }
