b454e220f4770a41b3ab908b591d7c546b83cef8
Dan <57754312+swarrenb@users.noreply.github.com>
2020-04-05 18:16:06 +0400

    Notifications fix #2#2

---
+++ src/main/webapp/precache-manifest.d9b7006bc34529e76f145f934f6952f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76b4f0668ec2ac3d76d363de96e163f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29a75baa20e7ead9dfb9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "69fd15f7e585fdf11f95",
+    "url": "/static/js/2.dcc88a02.chunk.js"
+  },
+  {
+    "revision": "ba1665840a4211d22917503a2a5544f6",
+    "url": "/static/js/2.dcc88a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a75baa20e7ead9dfb9",
+    "url": "/static/js/main.da993e5c.chunk.js"
+  },
+  {
+    "revision": "4e3dc7e752b1e8384f01",
+    "url": "/static/js/runtime-main.e12f6c60.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
